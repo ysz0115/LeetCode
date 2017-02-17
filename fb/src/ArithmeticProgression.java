82365937f55a3803c60e7d22232394b6f4ca934d
kelurysz <ysz0115@kelur.com>
2017-02-17 16:39:14 -0500

    Change user test

---
+++ fb/src/ArithmeticProgression.java
@@ -0,0 +1,13 @@
+/**
+ * Created by YSZ on 2017/2/17.
+ */
+public class ArithmeticProgression {
+
+    private int findLength(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                
+            }
+        }
+    }
+}
